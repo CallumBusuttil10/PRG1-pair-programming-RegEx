86aba7813ebcfef9e56c9a98a425dac9cd25c60b
CallumBusuttil10 <166800341+CallumBusuttil10@users.noreply.github.com>
2024-04-18 13:45:48 +0000

    Functions created and tested

--- regex.js
+++ regex.js
@@ -1,18 +1,43 @@
-//Some sample Regular expressions to have a play with
+const validWholeNumberRegEg = /^[0-9]+$/;
+// console.log(validWholeNumberRegEg.test(456));
+// console.log(validWholeNumberRegEg.test(`4`));
+// console.log(validWholeNumberRegEg.test(4.4));
+// console.log(validWholeNumberRegEg.test(`Error!`));
 
+const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,4}$/;
 const validWholeNumberRegEx = /^[0-9]+$/;
 const validPositiveWholeDecimalNumber = /^(?:\d+|\d*\.\d+)$/;
-const validAlphabeticString = /^[A-Za-z]+$/;
-
-// Use the test method to check whether something matches the rules. 
-console.log(validWholeNumberRegEx.test(456))
-
-// Create three functions which check user input for three pieces of data.
-// 1. Their name; 2. Their age; 3. Their height. 
 
 
+function emailValidity(email){
+    const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,4}$/;
+    return emailRegEx.test(email);
+}
 
+// console.log(emailValidity(`callum@gmail.com`));
+// console.log(emailValidity(`callum gmail.com`));
 
-// Other regex methods do exist such as match() (which looks for matching
-// characters) and replace() (which replaces characters), but test is 
-// probably the most commonly used and the one you should use here.
+function nameChecker(userName){
+    const nameRegEx = validAlphabeticString = /^[A-Za-z]+$/;
+    return nameRegEx.test(userName);
+}
+
+// console.log(nameChecker(`Cal`));//Should return true
+// console.log(nameChecker(`Cal2`));//Should return false
+
+function ageChecker(userAge){
+    const validWholeNumberRegEx = /^[0-9]+$/;
+    return validWholeNumberRegEx.test(userAge);
+}
+
+// console.log(ageChecker(26)); //Should return true
+// console.log(ageChecker(26.1)); //Should return false
+
+function heightChecker(userHeight){
+    const validPositiveWholeDecimalNumber = /^(?:\d+|\d*\.\d+)$/;
+    return validPositiveWholeDecimalNumber.test(userHeight);
+}
+
+// console.log(heightChecker(`12.3`)); //Should return true
+// console.log(heightChecker(12.3)); //Should return true
+// console.log(heightChecker(-12.3)); //Should return false
